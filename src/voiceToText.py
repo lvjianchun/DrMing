6f343f3c0c941aa3a894663982f2b1e307bef09e
yuezhao <yuezhao111@gmail.com>
2017-05-06 12:44:00 -0700

    Update voicetoText to a function

--- src/voiceToText.py
+++ src/voiceToText.py
@@ -1,16 +1,21 @@
-import speech_recognition as sr
+def GetInputVoiceToText():
+    import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
 
-BING_KEY = "fbd3d5eb1eaf443485cb3e568a132b86"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    text = r.recognize_bing(audio, key=BING_KEY)
-    print("Microsoft Bing Voice Recognition thinks you said " + text)
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+    BING_KEY = "fbd3d5eb1eaf443485cb3e568a132b86"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+    try:
+        text = r.recognize_bing(audio, key=BING_KEY)
+        print("Microsoft Bing Voice Recognition thinks you said " + text)
+        return text
+    except sr.UnknownValueError:
+        raise Exception("Microsoft Bing Voice Recognition could not understand audio")
+    except sr.RequestError as e:
+        raise Exception("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+
+if __name__ == '__main__':
+    s = GetInputVoiceToText()
