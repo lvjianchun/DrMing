4d9b9bbd4a2bda40d5bb737cb2fa0ad4cb279a33
Jianchun Lyu <ljc@Jianchuns-MacBook-Pro.local>
2017-05-06 20:57:53 -0700

    fix bug

--- Demo.py
+++ Demo.py
@@ -9,6 +9,7 @@ knowledgebase = loadBinaryTree.load()
 
 
 def say(text):
+	#os.system("say '%s' --voice Samantha" % text)
 	textToVoice.TextToSpeech(text)
 
 def getUserVoiceInput():
@@ -18,19 +19,22 @@ def main():
 	while True:
 		try:
 			say("Hello! How are you today?")
-			#text = getUserVoiceInput()
-			text = "I am wheezing. Help me"
-			say("We are pretending you said %s" % text)
-
+			text = getUserVoiceInput()
+			if not text:
+				say("Let's start over again")
+				continue
+			#text = "I am wheezing. Help me"
+			#say("We are pretending you said %s" % text)
 			response = wit.send(text)
 			parser = response_parser.responseParser(response)
-			intent, conf = parser.get("intent")
-			if intent == 'descOfSymptom' and conf > 0.75:
-				#root = knowledge_base.get_decision_tree()#(parser.get("symptom"))
-				root = loadBinaryTree.get_by_symptom("wheezing")
-				process_decision_tree(root)
-				say("Hope you have a good day! See ya")
-				break
+			if parser.has("intent"):
+				intent, conf = parser.get("intent")
+				if intent == 'descOfSymptom' and conf > 0.75:
+					#root = knowledge_base.get_decision_tree()#(parser.get("symptom"))
+					root = loadBinaryTree.get_by_symptom("wheezing")
+					process_decision_tree(root)
+					say("Hope you have a good day! See ya")
+					break
 			else:
 				say("I cross checked with Alexa and Siri, but we all don't understand. Let's try again")
 				continue
@@ -48,14 +52,14 @@ def process_decision_tree(root):
 	while loadBinaryTree.IsLeafNode(root) is False:
 		#question_to_ask = root.question
 		question_to_ask = loadBinaryTree.get_question(root)
-		print("Asking (%s)" % question_to_ask)
+		print("[DEBUG] ask (%s)" % question_to_ask)
 		say("Please answer after Beeeep... %s" % question_to_ask)
 		is_acceptable_answer = False
 		while not is_acceptable_answer:
 			res = getUserVoiceInput()
 			if res:
 				# small hack to narrow error
-				say("User said: %s " % res)
+				print("[DEBUG]User said: %s" % res)
 				response = mini_wit.send(res)
 				p = response_parser.responseParser(response)
 				res = 'yes' if p.has("positive") else res
@@ -68,7 +72,7 @@ def process_decision_tree(root):
 
 		root = root.Yes if is_acceptable_answer and 'yes' in res else root.No
 
-	say(root.question)
+	say("Dr. Ming suggested you to %s" % loadBinaryTree.get_question(root))
 
 if __name__ =='__main__' :
 	main()
